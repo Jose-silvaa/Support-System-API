5512695997be1dfb4d946f133965da9ba73cc850
Jose-silvaa <jvitorfacanha@gmail.com>
2026-04-05 10:59:57 -0300

    fix: Restrict task status changes for non-admin users

--- Support_System_API/Dtos/Ticket/UpdateTicketDto.cs
+++ Support_System_API/Dtos/Ticket/UpdateTicketDto.cs
@@ -6,6 +6,6 @@ public class UpdateTicketDto
 {
     public string? Title { get; set; }
     public string? Description { get; set; }
-    public TicketStatus? Status { get; set; }
+    public TicketStatus Status { get; set; }
 
 }
--- Support_System_API/Services/Interfaces/Ticket/ITicketService.cs
+++ Support_System_API/Services/Interfaces/Ticket/ITicketService.cs
@@ -7,7 +7,7 @@ namespace Support_System_API.Services.Interfaces.Ticket;
 public interface ITicketService
 {
     Task CreateTicket(CreateTicketDto request, Guid userId);
-    Task<Result<TicketResponseDto>> UpdatedTicket(UpdateTicketDto request, Guid ticketId, Guid userId);
+    Task<Result<TicketResponseDto>> UpdatedTicket(UpdateTicketDto request, Guid ticketId, Guid userId, String role);
     Task<bool> DeletedTicket(Guid id);
     Task<List<TicketListDto>> GetTicketsAsync(Guid userId, string role);
 }
